a8c14a655741f96735a73d31b7a84a6332a01fe8
duongnguyen <duongnt@fds.vn>
2022-10-14 16:29:13 +0700

    fix

--- test/test_blank.py
+++ test/test_blank.py
@@ -5,4 +5,4 @@ import blank
 def test_blank_package():
     my_str = "duongnguyen1996"
     rev_str = blank.reverse(my_str)
-    assert rev_str == "ofni.gnhnim"
+    assert rev_str == "6991neyugngnoud"
